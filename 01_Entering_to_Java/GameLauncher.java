3a76ece5656d1a8244ebcbbe24d7ad50ebeb6ed9
gerilewy@student.21-school.ru <gerilewy@student.21-school.ru>
2026-06-10 23:47:35 +0300

    ok

--- 01_Entering_to_Java/GameLauncher.java
+++ 01_Entering_to_Java/GameLauncher.java
@@ -1,3 +1,6 @@
+package GameLauncher
+import GuessGame
+
 class GameLauncher {
     public static void main(String[] args) {
         GuessGame game = new GuessGame();
---
+++ 01_Entering_to_Java/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("All world!");
+        System.out.println("In my hands");
+    }
+
+}
