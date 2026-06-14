a40186fbd5defd3adf14175b38738b67e4e73350
gerilewy@student.21-school.ru <gerilewy@student.21-school.ru>
2026-06-15 01:25:53 +0300

    ok

---
+++ 04_Objects_behave/Defaults.java
@@ -0,0 +1,19 @@
+public class Defaults {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println("Значение по умолчанию для int: " + x);
+
+        double d = 0.0;
+        System.out.println("Значение по умолчанию для double: " + d);
+
+        boolean b = false;
+        System.out.println("Значение по умолчанию для boolean: " + b);
+
+        char c = '\u0000';
+        System.out.println("Значение по умолчанию для char: '" + c + "'");
+
+        String s = null;
+        System.out.println("Значение по умолчанию для String: " + s);
+    }
+
+}
